a5767389525ce7afa6a8b0c2229cf1a4593ce8aa
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-01 23:02:45 +0600

    Updated contact page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -6,61 +6,23 @@ const ContactUs = () => {
       <Helmet>
      <title>Contact Us</title>
   </Helmet>
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-semibold text-center mb-8">Contact Us</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 ">
-          <div>
-            <form className="md:w-1/2 lg:ml-10">
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                  placeholder="Enter your name"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                  placeholder="Enter your email"
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700">
-                  Your Message
-                </label>
-                <textarea
-                  name="message"
-                  rows="4"
-                  className="mt-1 p-2 border border-gray-300 rounded-md w-full"
-                  placeholder="Enter your message"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
-              >
-                Send Message
-              </button>
-            </form>
+      <div className="container mx-auto my-7 md:my-14">
+      <h3 className="text-3xl  md:text-5xl font-bold text-center mb-2 md:mb-4">
+          Contact <span className="text-[#2F71FF]">Us!</span>
+        </h3>
+        <p className="text-sm md:text-xl text-center text-gray-600">Let us know your words</p>
+        <div className="flex justify-center gap-6 mt-6">
+          <div className="bg-white p-6">
+          <h2 className="text-3xl text-center mb-4"><ion-icon name="location-outline"></ion-icon></h2>
+          <h2 className="text-xl text-gray-600">Bangladesh, Chattogram</h2>
           </div>
-          <div className="w-1/2">
-            <h3 className="text-xl font-semibold mb-4">Contact Information</h3>
-            <p className="text-gray-700 mb-2">
-              <span className="font-semibold">Email:</span>{" "}
-              web.dev.dinar@gmail.com
-            </p>
-            <p className="text-gray-700">
-              <span className="font-semibold">Phone:</span> 01889112836
-            </p>
+          <div className="bg-white p-6">
+          <h2 className="text-3xl text-center mb-4"><ion-icon name="call-outline"></ion-icon></h2>
+          <h2 className="text-xl text-gray-600">+01495254323</h2>
+          </div>
+          <div className="bg-white p-6">
+          <h2 className="text-3xl text-center mb-4"><ion-icon name="mail-outline"></ion-icon></h2>
+          <h2 className="text-xl text-gray-600">VoteSnap@gmail.com</h2>
           </div>
         </div>
       </div>
