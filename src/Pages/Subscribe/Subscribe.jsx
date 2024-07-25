e6bb3962fb5f691658442fb84ef179f7678827fb
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-07-25 20:52:38 +0600

    Updated newsletter section responsive ui design

--- src/Pages/Subscribe/Subscribe.jsx
+++ src/Pages/Subscribe/Subscribe.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Subscribe = () => {
     return (
-        <div className='px-4 md:px-16 my-6 md:my-12 flex-col md:flex-row justify-center md:justify-between flex h-60 bg-opacity-30 items-center rounded-lg border'>
+        <div className='px-4 md:px-16 mx-4 md:mx-auto my-6 md:my-12 flex-col md:flex-row justify-center md:justify-between flex h-72 bg-opacity-30 items-center rounded-lg border'>
             <div className=''>
                 <h3 className='text-2xl md:text-4xl font-bold mb-2'>Subscribe for Newsletters</h3>
                 <p className=''>Get E-mail updates about our latest surveys</p>
