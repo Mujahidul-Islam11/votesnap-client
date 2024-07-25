21e0daf51d4dc412ad8350b3360e82f1f3ad879c
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-07-25 20:50:44 +0600

    Updated the registration form responsive ui design

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
               <img
                 src="https://i.postimg.cc/CLhjNvgJ/676-removebg-preview.png"
                 alt=""
-                className="w-28 h-20"
+                className="w-28 h-[65px] md:h-20"
               />{" "}
           </div>
         </NavLink>
