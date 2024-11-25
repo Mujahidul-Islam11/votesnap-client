0b94b5403c4ba7eb7669776022b2f5b7900c51aa
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-11-25 22:50:25 +0600

    Updated newsletter section

--- src/Pages/Subscribe/NewsLetter.jsx
+++ src/Pages/Subscribe/NewsLetter.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const NewsLetter = () => {
     return (
-        <div className='px-4 md:px-16 mx-4 md:mx-auto my-6 md:my-12 flex-col md:flex-row justify-center md:justify-between flex h-72 bg-gradient-to-r from-[#2f71ff5a] to-[#2f71ffd6] items-center rounded-lg border'>
-            <div className=''>
-                <h3 className='text-2xl md:text-4xl font-bold mb-2'>Subscribe for Newsletters</h3>
-                <p className=''>Get E-mail updates about our latest surveys</p>
+        <div className='w-full border py-14'>
+            <div className='flex flex-col md:flex-row gap-6 md:justify-between items-center h-56 md:h-80 max-w-7xl mx-auto md:px-12'>
+            <div className='text-center'>
+                <h3 className='text-4xl font-extralight mb-2 text-gray-600'>Sign up now</h3>
+                <p className='text-[22px] text-gray-500 font-extralight'>Free unlimited surveys, <br /> questions and responses.</p>
             </div>
             <div className='mt-6 md:mt-0 flex-row flex items-center'>
-                <input placeholder='Enter email to get started' type="text" className='w-[150px] md:w-[400px] newsLetterInput outline-[#2F71FF] py-2 md:py-3 px-4 md:px-6 rounded-l-lg border-none'/>
-                <button className='bg-[#2F71FF] text-white border-none hover:bg-[#2f71ffbf] py-2 md:py-3 px-4 md:px-6 rounded-r-lg '>Subscribe</button>
+                <input placeholder='Email Address' type="text" className='w-[150px] md:w-[400px] newsLetterInput outline-[#2F71FF] py-2 md:py-3 px-4 md:px-4 rounded-l-md border black'/>
+                <button className='bg-[#2F71FF] text-white border-none hover:bg-[#2f71ffbf] py-2 md:py-3 px-4 md:px-12 rounded-r-md '>Get Started </button>
             </div>
+        </div>
         </div>
     );
 };
