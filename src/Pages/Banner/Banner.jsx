36ebfdaa018305039ba056ac8646482939cbf258
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-12-02 21:54:31 +0600

    Updated banner ui

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -10,19 +10,19 @@ const Banner = () => {
   };
 
   return (
-    <div className="hero max-w-7xl mx-auto md:px-12 text-gray-900 md:py-14 xl:py-16">
-      <div className="hero-content bg-transparent h-full flex-col lg:flex-row-reverse">
+    <div className="pb-6 border-b">
+      <div className="max-w-7xl mx-auto px-6 md:px-12 bg-transparent flex flex-col lg:flex-row-reverse justify-center items-center">
+        <div className="md:w-[800px]">
         <Lottie options={defaultOptions}></Lottie>
+        </div>
         <div>
-          <h1 className="max-w-2xl text-4xl md:text-5xl xl:text-6xl font-extrabold">
+          <h1 className="text-3xl font-bold text-dc">
             Empower Your Voice, Shape the <span className="text-[#2F71FF]">Future!</span>
           </h1>
-          <p className="py-6 md:text-lg lg:text-xl">
-            Welcome to VoteSnap, where your opinions matter. Explore a world of
-            surveys, polls, and insights, and be part of a community that values
-            your voice. Explore surveys now!
+          <p className="py-6 md:text-3xl text-sdc font-light">
+          Create free online surveys <br /> with unlimited questions and responses!
           </p>
-          <button className="btn btn-primary bg-[#2F71FF] text-white border-none hover:bg-[#2f71ffbf]">Get Started</button>
+          <button className="btn btn-primary bg-[#2F71FF] px-8 md:px-12 text-white border-none hover:bg-[#2f71ffbf] flex items-center gap-2">Get Started <span className=" text-lg font-semibold">&#8594;</span></button>
         </div>
       </div>
     </div>
